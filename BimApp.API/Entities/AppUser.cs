0ff8c0a0352e5edd21cbc9b02d6892be72233069
Junaid Malik <junmalik109@gmail.com>
2021-05-20 16:40:23 +0100

    [Entities[ Updated user data model

--- BimApp.API/Entities/AppUser.cs
+++ BimApp.API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BimApp.API.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,5 +15,32 @@ namespace BimApp.API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string About { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
