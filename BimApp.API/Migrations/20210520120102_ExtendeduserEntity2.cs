a7e4e57f7546a3d8a8f94beb1ec3891a40392894
Junaid Malik <junmalik109@gmail.com>
2021-05-20 16:41:17 +0100

    [Migrations] EF Core database migration added

---
+++ BimApp.API/Migrations/20210520120102_ExtendeduserEntity2.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BimApp.API.Migrations
+{
+    public partial class ExtendeduserEntity2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photo_Users_AppUserId",
+                table: "Photo");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Photo",
+                table: "Photo");
+
+            migrationBuilder.RenameTable(
+                name: "Photo",
+                newName: "Photos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Photo_AppUserId",
+                table: "Photos",
+                newName: "IX_Photos_AppUserId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Photos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Photos",
+                table: "Photos",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Users_AppUserId",
+                table: "Photos",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Users_AppUserId",
+                table: "Photos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Photos",
+                table: "Photos");
+
+            migrationBuilder.RenameTable(
+                name: "Photos",
+                newName: "Photo");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photo",
+                newName: "IX_Photo_AppUserId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Photo",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Photo",
+                table: "Photo",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photo_Users_AppUserId",
+                table: "Photo",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
