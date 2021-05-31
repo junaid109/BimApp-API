1729678dee91833cbe0ed118200ba4ff74641738
Junaid Malik <junmalik109@gmail.com>
2021-05-31 15:34:34 +0100

    [Models] user data models added

---
+++ BimApp.API/Dtos/MemberDto.cs
@@ -0,0 +1,41 @@
+﻿using BimApp.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BimApp.API.Dtos
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public int Age { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string About { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<PhotoDto> Photos { get; set; }
+
+
+    }
+}
