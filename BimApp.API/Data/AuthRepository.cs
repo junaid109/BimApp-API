12760f0422db75c77f98e98da6c4a65f77c6eaca
Junaid Malik <junmalik109@gmail.com>
2022-09-07 11:39:57 +0100

    PhotoService for photo upload

--- BimApp.API/Data/AuthRepository.cs
+++ BimApp.API/Data/AuthRepository.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
-using System.Threading.Tasks;
+  using System.Threading.Tasks;
 
 namespace BimApp.API.Data
 {
